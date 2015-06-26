1b6f8970194124e041e7c0845cd00a4f2aa5efb9
Adonis <adonis.settouf@gmail.com>
2015-06-26 16:16:20 +0200

    First commit

---
+++ public/javascripts/global.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function{
+	
+});
+
+
+
+//We do not want IE users here!
+var forbidIE =  function(){
+	var ua = window.navigator.userAgent;
+	var MSIE = ua.indexOf('MSIE');
+	if (MSIE != -1)  {
+		document.documentElement.innerHTML='';
+		alert("Use a true browser");
+		window.location.href = "https://www.mozilla.org/en-US/firefox/new/";
+	}
+};
+
+forbidIE();
